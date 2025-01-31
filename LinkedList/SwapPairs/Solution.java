daadc8ccc752acda15237d9b90413f242317a561
Brandon Paulsen <bspaulsen@dons.usfca.edu>
2025-01-31 15:07:13 -0800

    Added iterative solution

--- LinkedList/SwapPairs/Solution.java
+++ LinkedList/SwapPairs/Solution.java
@@ -11,7 +11,8 @@ class Solution {
             this.next = next;
         }
     }
-    public ListNode swapPairs(ListNode head) {
+
+    public ListNode swapPairs(ListNode head) { //RECURSIVE
         if(head == null || head.next == null) {
             return head;
         }
@@ -19,5 +20,27 @@ class Solution {
         head.next = swapPairs(newHead.next);
         newHead.next = head;
         return newHead;
+    }
+
+    public ListNode swapPairs(ListNode head) { //Iterative
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode p = head;
+        ListNode c = p.next;
+        ListNode n = c.next;
+        head = c;
+        while(c != null) {
+            c.next = p;
+            if(n!= null && n.next != null) {
+                p.next = n.next;
+            } else {
+                p.next = n;
+            }
+            p = n;
+            c = n == null ? null : n.next;
+            n = c == null ? null : c.next;
+        }
+        return head;
     }
 }

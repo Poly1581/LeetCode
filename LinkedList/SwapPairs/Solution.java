22c8cb83129aff4c5d1b4c8ade501f2bb43c0647
Brandon Paulsen <bspaulsen@dons.usfca.edu>
2025-01-31 14:46:33 -0800

    Finished Swap nodes in pairs
    The solution is O(n), but uses recursion and therefore a lot of memory - I intend to rework this to avoid recursion.

---
+++ LinkedList/SwapPairs/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = head.next;
+        head.next = swapPairs(newHead.next);
+        newHead.next = head;
+        return newHead;
+    }
+}

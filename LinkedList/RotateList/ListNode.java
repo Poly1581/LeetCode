0295ff7c3565aa4d05db07ddda23531bdee28efe
Brandon Paulsen <bspaulsen@dons.usfca.edu>
2025-01-31 09:46:36 -0800

    modified structure

--- LinkedList/RotateList/ListNode.java
+++ LinkedList/RotateList/ListNode.java
@@ -3,6 +3,7 @@ public class ListNode {
     ListNode next;
     ListNode(int val) {
         this.val = val;
+        this.next = null;
     }
     ListNode(int val, ListNode next) {
         this.val = val;
--- LinkedList/RotateList/RotateList.java
+++ LinkedList/RotateList/RotateList.java
@@ -1,5 +1,28 @@
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
+        // Do nothing on empty list
+        if(head.next == null) {
+            return head;
+        }
 
+        // Make loop
+        ListNode l = head;
+        while(l.next != null) {
+            l = l.next;
+        }
+        l.next = head;
+
+        //  Shift pointers
+        ListNode p = head;
+        ListNode c = head.next;
+        while(k > 0) {
+            k--;
+            p = p.next;
+            c = c.next;
+        }
+
+        // Rotate
+        p.next = null;
+        return c;
     }
 }
